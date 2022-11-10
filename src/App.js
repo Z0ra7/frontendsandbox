a3ad9a411ad10288249916f8020bab5230b6fead
steve <steven.meyer@students.ffhs.ch>
2022-11-10 20:23:39 +0100

    frontend added clickon event and fixing date issue

--- src/App.js
+++ src/App.js
@@ -3,81 +3,129 @@ import './App.css';
 //Importing FullCalendar Module
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-//import timeGridPlugin from '@fullcalendar/timegrid'
-//import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 //import { INITIAL_EVENTS, createEventId, getEvents } from './event_utils'
 //Importing axios service
 import axios from 'axios';
+//import { INITIAL_EVENTS } from './event_utils';
 class App extends React.Component {
   //initialize array variable
   constructor() {
     //super is used to access the variables
     super();
     this.state = {
-       event: [],
-       user: []
+      event: [],
+      user: []
     }
- }
+  }
 
- componentDidMount() {
- //API request
- //if(this.stateLoaded)
-//{return;
-//}
-
-
-  //this.stateLoaded = 
-  axios.get("http://localhost:8080/api/event").then(response => {
-
- // getting and setting api data into variable
-  
-
- var data = [];
-
-var titleSan = 'Randomness';
-for (let i = 0; i<response.data.length; i++){
-  let obj = response.data[i];
-  data.push({
-  id: obj.eventId,
-  title: titleSan,
-  start: new Date(obj.start).toISOString(),   //.replace(/T.*$/, '')
-  end: new Date(obj.start).toISOString()+ 'T03:00:00'
-});
-this.setState({ event : data}); //,user: variable für userstate
-//console.log('hey')
-//console.log(this.jsonArr);
-  //console.log(obj.eventId)
-  //console.log(new Date(obj.start).toISOString().replace(/T.*$/, ''))
-}
+  componentDidMount() {
+    //API request
+    //if(this.stateLoaded)
+    //{return;
+    //}
 
 
-})
-}
-  
-//Final output
-render() {
+    //this.stateLoaded = 
+    axios.get("http://localhost:8080/api/event").then(response => {
 
- console.log(this.state.event);
-  return (
-    <div className="App">
-      
+      // getting and setting api data into variable
+
+
+      var data = [];
+
+      var titleSan = 'Randomness';
+      for (let i = 0; i < response.data.length; i++) {
+        let obj = response.data[i];
+
+        data.push({
+
+          id: obj.eventId,
+          title: titleSan,
+          start: new Date(obj.start),
+          end: new Date(obj.end)
+        });
+        this.setState({ event: data }); //,user: variable für userstate
+      }
+
+
+    })
+
+    const article = { title: 'React PUT Request Example' };
+    axios.put('https://reqres.in/api/articles/1', article)
+      .then(response => this.setState({ updatedAt: response.data.updatedAt }));
+  }
+  handleEventClick = (clickInfo) => {
+    if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove()
+    }
+  }
+
+
+  handleDateClick = arg => {
+    if (window.confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
+      this.setState({
+        // add new event data
+        event: this.state.event.concat({
+          // creates a new array
+          title: "New Event",
+          start: arg.date,
+          allDay: arg.allDay
+        })
+      });
+    }
+  };
+
+  //Final output
+  render() {
+
+    return (
+      <div className="App">
+
         <h1>SSE Terminkalender</h1>
-      
+
         <FullCalendar
-          plugins={[ dayGridPlugin ]}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          eventTimeFormat={{
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+          }}
+          slotLabelFormat={{
+            hour: 'numeric',
+            minute: '2-digit',
+            hour12: false
+
+          }}
           headerToolbar={{
             left: 'prev,next today',
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay'
           }}
+
           initialView="dayGridMonth"
-          events = {this.state.event}
+          events={this.state.event}
+          eventsSet={this.handleEvents}
+        
+          eventContent={renderEventContent}
+          dateClick={this.handleDateClick}
+          eventClick={this.handleEventClick}
         />
-    </div>
-  );
-  
+      </div>
+    );
+
+  }
+
 }
 
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b><b></b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  )
 }
 
 export default App;
--- src/event_utils.js
+++ src/event_utils.js
@@ -1,5 +1,5 @@
 
-/*let eventGuid = 0
+let eventGuid = 0
 let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
 const api_url = "http://localhost:8080/api/event";
 
@@ -19,10 +19,10 @@ export const INITIAL_EVENTS = [
     {
         id: createEventId(),
         title: 'Timed event',
-        start: todayStr + 'T12:00:00'
+        start: todayStr + 'T22:00:00'
     }
 ]
 
 export function createEventId() {
     return String(eventGuid++)
-}*/
+}
