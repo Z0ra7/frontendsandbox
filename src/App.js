b030adaaaaf25a594db6ebe637bffc19a73c4bc8
steve <steven.meyer@students.ffhs.ch>
2022-11-12 23:03:49 +0100

    implemented drag and drop, modal view,eventclick

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {} from 'react';
 import './App.css';
 //Importing FullCalendar Module
-import FullCalendar from '@fullcalendar/react'
+import FullCalendar, { CalendarApi} from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import "@fullcalendar/daygrid/main.css"
+import "@fullcalendar/timegrid/main.css"
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 //import { INITIAL_EVENTS, createEventId, getEvents } from './event_utils'
 //Importing axios service
 import axios from 'axios';
@@ -16,10 +21,14 @@ class App extends React.Component {
     super();
     this.state = {
       event: [],
-      user: []
+      currentEvent: {},
+      user: [],
+      modal: false,
     }
+    //this.handleEventClick = this.handleEventClick.bind(this);
+
   }
-  
+
   componentDidMount() {
     //API request
     //if(this.stateLoaded)
@@ -35,7 +44,6 @@ class App extends React.Component {
 
       var data = [];
 
-      var titleSan = 'Randomness';
       for (let i = 0; i < response.data.length; i++) {
         let obj = response.data[i];
 
@@ -43,22 +51,52 @@ class App extends React.Component {
           id: obj.eventId,
           title: obj.eventType,
           start: new Date(obj.start),
-          end: new Date(obj.end)
+          end: new Date(obj.end),
+          allDay:obj.allDay
         });
         this.setState({ event: data }); //,user: variable für userstate
+        console.log(this.state.event)
       }
 
 
     })
 
   }
+  toggle = () => {
+
+    this.setState({ modal: !this.state.modal });
+  };
+
+
+
   handleEventClick = (clickInfo) => {
-    if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-      clickInfo.event.remove()
+    //console.log(eventInfo.event.end)
+//var event = FullCalendar.CalendarApi.getEventById(clickInfo.event.id);
+
+    console.log(clickInfo.event.id)
+    const currentEventObject = { title: clickInfo.event.title, allDay: clickInfo.event.allDay, start: clickInfo.event.start.toString() };
+    if (clickInfo.event.end != null) {
+      currentEventObject.end = clickInfo.event.end.toString();
     }
+    console.log(clickInfo.event.end)
+    //console.log(currentEventObject)
+    this.setState({ currentEvent: currentEventObject })
+
+    this.toggle();
+    //if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+
+    //clickInfo.event.remove()
+    //}
   }
 
- 
+  //handleEvents = (events) => {
+  // this.setState({
+  //  currentEvents: events
+  //})
+  //}
+
+
+
 
   handleDateClick = arg => {
     if (window.confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
@@ -70,38 +108,46 @@ class App extends React.Component {
           start: arg.date,
           allDay: arg.allDay
         })
-       
+
       });
     }
-    function subtractHours(date, hours) {
-      date.setHours(date.getHours() - hours);
-    
-      return date;
-    }
-    function addHours(date, hr){
-      date.setHours(hr);
-      return date;
-  }
 
-  //console.log(arg.date.strftime('%d-%m-%Y %H:%M'))
-    console.log(arg.date.toISOString().replace(/T/, ' '))
-    var Date = addHours(arg.date,1)
-    console.log(Date);
-    console.log(Date.toISOString().replace(/T/, ' '));
+
+    //console.log(arg.date.strftime('%d-%m-%Y %H:%M'))
+    // console.log(arg.date.toISOString().replace(/T/, ' '))
+    //console.log(Date);
+    //console.log(Date.toISOString().replace(/T/, ' '));
 
     axios.post('http://localhost:8080/api/event', {
       id: null,
-      title: 1,
+      eventType: 1,
       start: arg.date.toISOString().replace(/T/, ' ').replace(/\..+/, ''),
       end: arg.date.toISOString().replace(/T/, ' ').replace(/\..+/, '')
-    //data
 
     })
   };
 
+  handleDateSelect = (selectInfo) => {
+    let title = prompt('Please enter a new title for your event')
+    let calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id:
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      })
+    }
+  }
+
+
+
   //Final output
   render() {
-console.log(this.state.event)
     return (
       <div className="App">
 
@@ -127,14 +173,43 @@ console.log(this.state.event)
           }}
 
           initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
           events={this.state.event}
           eventsSet={this.handleEvents}
-        
-          eventContent={renderEventContent}
+          select={this.handleDateSelect}
+
+          eventContent={this.renderEventContent}
           dateClick={this.handleDateClick}
           eventClick={this.handleEventClick}
-
         />
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+        >
+          <ModalHeader toggle={this.toggle}>
+            Edit Event: {this.state.currentEvent.title}
+          </ModalHeader>
+          <ModalBody>
+            <div>
+              Title: {this.state.currentEvent.title}
+              <br></br><br></br>
+              Start: {this.state.currentEvent.start}
+              <br></br><br></br>
+              End: {this.state.currentEvent.end}
+              <br></br><br></br>
+
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary">Do Something</Button>{" "}
+            <Button color="secondary" onClick={this.toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
       </div>
     );
 
@@ -142,13 +217,5 @@ console.log(this.state.event)
 
 }
 
-function renderEventContent(eventInfo) {
-  return (
-    <>
-      <b>{eventInfo.timeText}</b><b></b>
-      <i>{eventInfo.event.title}</i>
-    </>
-  )
-}
 
 export default App;
